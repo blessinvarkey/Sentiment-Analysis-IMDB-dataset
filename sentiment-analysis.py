de011690d47ac10f092a9c1e51fedede0f5df113
Blessin Varkey <blessinvarkey@gmail.com>
2021-02-07 00:52:13 +0530

    update .py

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 import glob
 
-def read_imdb_data(data_dir='../data/aclImdb'):
+def read_imdb_data(data_dir='aclImdb'):
     data = {}
     labels = {}
 
@@ -28,32 +28,3 @@ def read_imdb_data(data_dir='../data/aclImdb'):
                     "{}/{} data size does not match labels size".format(data_type, sentiment)
 
     return data, labels
-
-data, labels = read_imdb_data()
-print("IMDB reviews: train = {} pos / {} neg, test = {} pos / {} neg".format(
-            len(data['train']['pos']), len(data['train']['neg']),
-            len(data['test']['pos']), len(data['test']['neg'])))
-
-from sklearn.utils import shuffle
-
-def prepare_imdb_data(data, labels):
-    """Prepare training and test sets from IMDb movie reviews."""
-
-    #Combine positive and negative reviews and labels
-    data_train = data['train']['pos'] + data['train']['neg']
-    data_test = data['test']['pos'] + data['test']['neg']
-    labels_train = labels['train']['pos'] + labels['train']['neg']
-    labels_test = labels['test']['pos'] + labels['test']['neg']
-
-    #Shuffle reviews and corresponding labels within training and test sets
-    data_train, labels_train = shuffle(data_train, labels_train)
-    data_test, labels_test = shuffle(data_test, labels_test)
-
-    # Return a unified training data, test data, training labels, test labets
-    return data_train, data_test, labels_train, labels_test
-
-
-train_X, test_X, train_y, test_y = prepare_imdb_data(data, labels)
-print("IMDb reviews (combined): train = {}, test = {}".format(len(train_X), len(test_X)))
-
-#train_X[100]
