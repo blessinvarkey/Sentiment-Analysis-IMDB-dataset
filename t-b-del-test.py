11fe540aac0ddd9e06cb2dad477beec635c8f5e8
Blessin Varkey <blessinvarkey@gmail.com>
2021-02-11 03:50:37 +0530

    Update t-b-del-test.py

--- t-b-del-test.py
+++ t-b-del-test.py
@@ -1,4 +1,3 @@
-
 class Gaussian():
 
     def __init__(self, mean, stdev):
@@ -7,10 +6,26 @@ class Gaussian():
         self.data = []
 
     def calculate_mean(self):
-        pass
+        self.data
+        self.mean = float(sum(self.data)/len(self.data))
+        return self.mean
+
 
     def calculate_stdev(self):
-        pass
+        j = 0
+        for i in range(len(self.data)):
+            result = self.mean - self.data[i]
+            squared = result*result
+            j = j + squared
+            print(result)
+            print(squared)
+        print('--Total---')
+        print(j)
+            value =  sum(squared)/len(squared)
+
+            self.stdev = (self.mean)*(1-)
+#        return self.stdev
+
 
     def read_data_file(self, file_name, sample = True):
         with open(file_name) as file:
@@ -21,6 +36,10 @@ class Gaussian():
                 line = file.readline()
         file.close()
 
+        self.data = data_list
+        self.mean = calculate_mean()
+        self.stdev = self.calculate_stdev(sample)
+
     def plot_histogram(self):
         pass
 
@@ -29,3 +48,19 @@ class Gaussian():
 
     def plot_histogram_pdf(self, n_spaces = 50):
         pass
+
+#magic methods
+    def __add__(self, other):
+        pass
+
+#magic methods
+    def __repr__(self):
+        pass
+
+
+    def main():
+        pass
+
+
+    if __name__ == '__main__':
+        main()
